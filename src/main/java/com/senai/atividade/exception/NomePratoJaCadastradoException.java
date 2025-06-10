9f2695ede988064f07f4e2feb662858b6967a7e1
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-06-10 11:11:00 -0300

    Delete src directory

--- src/main/java/com/senai/atividade/exception/NomePratoJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.senai.atividade.exception;
-
-public class NomePratoJaCadastradoException extends RuntimeException {
-    public NomePratoJaCadastradoException(String message) {
-        super(message);
-    }
-}
